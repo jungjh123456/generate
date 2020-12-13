24ca1e5cd5858c15a572bde3ceb8a5dbf65a5503
jungjh123456 <jungjh123456@naver.com>
2020-12-13 17:18:49 +0900

    fix css

--- public/js/app.js
+++ public/js/app.js
@@ -12,32 +12,19 @@ const $toggle = document.querySelector('.toggle');
 let state = true;
 
 
-[...$btn].forEach(item => item.onclick = e => {
-  e.target.nextElementSibling.style.transition = 'none';
-  console.log(e.target.nextElementSibling);
-  state = false;
-  [...$btn].forEach(item => {
+$button.onclick = e => {
+  console.log(e.target);
+    [...$btn].forEach(item => {
     item.nextElementSibling.style.transition = 'none';
     item.nextElementSibling.classList.remove('on');
     item.classList.remove('on');
   });
-
-  e.target.classList.add('on');
-
-    e.target.nextElementSibling.classList.add('on');
-    // e.target.nextElementSibling.style.transition = '1s';
-  
-  // if(item.classList.contains('on')) item.classList.remove('on')
-  // e.target.nextElementSibling.classList.toggle('on');
-  
-})
-$button.onclick = e => {
-  if (!e.target.classList.contains('on')) {
-    e.target.nextElementSibling.style.transition = '1s';
-  }
-  console.log(e.target)
-
+  e.target.classList.toggle('on');
+  e.target.nextElementSibling.classList.toggle('on');
 }
+
+
+
 window.onclick = e => {
   if (!e.target.matches('html')) return;
   removeElement();
@@ -45,7 +32,7 @@ window.onclick = e => {
 
 $searchBtn.onclick = e => {
   e.preventDefault();
-  console.log(e.target)
+  
   // if (state === true){
   // $searchInput.classList.add('active');
   // state = false
